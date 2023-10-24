c79784bcc379e80c7fcf5e098124c0672237957b
budanov <budanov@rozetka.ua>
2023-10-25 00:40:42 +0300

    finish lesson 1

--- lesson_1/src/makeIceCream.ts
+++ lesson_1/src/makeIceCream.ts
@@ -1,4 +1,7 @@
-// Визначаємо всі ціни морозива та начинок
+/**
+ * @const
+ * Прайс інгрідієнтів морозива
+ */
 const iceCreamParams = {
     sizeSmall: 10,
     sizeBig: 25,
@@ -8,7 +11,10 @@ const iceCreamParams = {
     marshmallow: 5
 };
 
-// Визначаємо розмір морозива із замовлення
+/**
+ * Метод запитує користувача про бажаний розмір морозива і повертає вартість обраної порції
+ * @return Вартість морозива
+ */
 function getSizeCost(): number {
     let size: string = prompt('You want big or small ice cream?');
     if (!size) {
@@ -22,18 +28,32 @@ function getSizeCost(): number {
     return size == 'small' ? iceCreamParams.sizeSmall: iceCreamParams.sizeBig;
 }
 
-// Запитуємо про начинку до морозива
+/**
+ * Метод запитує користувача про будь яку добавку що зазначена у iceCreamParams і повертає вартість добавки
+ * @param adds Визначеється як властивість з об'єкту iceCreamParams
+ * @return Вартість добавки
+ */
 function getAdds(adds: keyof typeof iceCreamParams): number {
     let isAdd = confirm(`Add ${adds} to ice cream?`);
     return isAdd ? iceCreamParams[adds]: 0;
 }
 
-// Підраховуємо вартість морозива
+/**
+ * Метод підсумовує вартість всіх інгридієнтів морозива і повертає загальну вартість
+ * @param iceCream {number} Вартість самого морозива
+ * @param isAddChocolate {number} Вартість добавки шоколад
+ * @param isAddCaramel {number} Вартість добавки карамель
+ * @param isAddBerries {number} Вартість добавки ягоди
+ * @param isAddMarshmallow {number} Вартість добавки маршмеллоу
+ * @return Загальна вартість морозива з добавками
+ */
 function calculateIceCreamCost(iceCream: number, isAddChocolate: number, isAddCaramel: number, isAddBerries: number, isAddMarshmallow: number): number {
     return iceCream + isAddChocolate + isAddCaramel + isAddBerries +isAddMarshmallow;
 }
 
-// Робимо морозиво для покупця =)
+/**
+ * Метод запускає процес опитування користувача і підрахунок вартості морозива
+ */
 function makeIceCream(): void {
     const size = getSizeCost();
     const isAddChocolate =  getAdds('chocolate');
